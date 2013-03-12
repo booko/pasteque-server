02a85a834de77f7c2b9a5c6ab355e8622596fac7
Cédric Houbart <cedric@scil.coop>
2013-03-12 08:11:25 +0100

    Restored places service

--- inc/data/models/Floor.php
+++ inc/data/models/Floor.php
@@ -18,6 +18,8 @@
 //    You should have received a copy of the GNU General Public License
 //    along with POS-Tech.  If not, see <http://www.gnu.org/licenses/>.
 
+namespace Pasteque;
+
 class Floor {
 
     public $id;
