bdf6efccf86ba91ca576da3a074f09eb70cc4ff6
Cédric Houbart <cedric@scil.coop>
2013-03-15 16:32:07 +0100

    Database module management (per user activation)

---
+++ core_modules/modules/database/module.php
@@ -0,0 +1,52 @@
+<?php
+//    Pastèque Web back office, Database module manager
+//
+//    Copyright (C) 2013 Scil (http://scil.coop)
+//
+//    This file is part of Pastèque.
+//
+//    Pastèque is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Pastèque is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Pastèque.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace Pasteque {
+    if (@constant("\Pasteque\ABSPATH") === NULL) {
+        die();
+    }
+}
+
+namespace DatabaseMM {
+    require_once(dirname(__FILE__) . "/config.php");
+    $data = NULL;
+    function getModules($user_id) {
+        global $data, $config;
+        if ($data == NULL) {
+            $pdo = \Pasteque\PDOBuilder::getPDO();
+            $stmt = $pdo->prepare("SELECT modules FROM " . $config['table'] .
+                " WHERE user_id = :id");
+            $stmt->bindParam(":id", $user_id, \PDO::PARAM_INT);
+            $stmt->execute();
+            if ($row = $stmt->fetch()) {
+                $modules = $row['modules'];
+                $data = explode(",", $modules);
+            }
+        }
+        return $data;
+    }
+}
+
+namespace Pasteque {
+    function get_loaded_modules($user_id) {
+        return \DatabaseMM\getModules($user_id);
+    }
+}
+?>
