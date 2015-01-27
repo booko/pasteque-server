ec596ebafc6919b631fa15f7995a1db205966b9b
Cédric Houbart <cedric@scil.coop>
2015-01-27 18:25:34 +0100

    Allow to save a ticket line with an undefined product

--- inc/data/api/TicketsAPI.php
+++ inc/data/api/TicketsAPI.php
@@ -150,7 +150,11 @@ class TicketsAPI extends APIService {
                 foreach ($jsonTkt->lines as $jsLine) {
                     // Get line info
                     $number = $jsLine->dispOrder;
-                    $productId = $jsLine->productId;
+                    if (property_exists($jsLine, "productId")) {
+                        $productId = $jsLine->productId;
+                    } else {
+                        $productId = null;
+                    }
                     $quantity = $jsLine->quantity;
                     $price = $jsLine->price;
                     $taxId = $jsLine->taxId;
@@ -181,8 +185,8 @@ class TicketsAPI extends APIService {
                     }
                     $product = ProductsService::get($productId);
                     $tax = TaxesService::getTax($taxId);
-                    if ($product == null || $tax == null) {
-                        $this->fail(new APIError("Unknown product or tax"));
+                    if ($tax == null) {
+                        $this->fail(new APIError("Unknown tax"));
                         break;
                     }
                     $newLine = new TicketLine($number, $product,
