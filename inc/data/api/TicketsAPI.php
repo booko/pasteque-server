6ac0271eb88665422d59a8b11c0ea4f72ac11ef9
Cédric Houbart <cedric@scil.coop>
2015-01-27 18:22:06 +0100

    Add details on error in TicketsAPI save

--- inc/data/api/TicketsAPI.php
+++ inc/data/api/TicketsAPI.php
@@ -134,6 +134,7 @@ class TicketsAPI extends APIService {
             }
             foreach ($json as $jsonTkt) {
                 if ($jsonTkt === null) {
+                    $this->fail(new APIError("Unable to decode ticket"));
                     break;
                 }
                 $userId = $jsonTkt->userId;
@@ -172,7 +173,7 @@ class TicketsAPI extends APIService {
                         $attrsId = TicketsService::createAttrSetInst($attrs);
                         
                         if ($attrsId === false) {
-                            // Fail, will check line count to continue
+                            $this->fail(new APIError("Unknown attributes"));
                             break;
                         }
                     } else {
@@ -181,6 +182,7 @@ class TicketsAPI extends APIService {
                     $product = ProductsService::get($productId);
                     $tax = TaxesService::getTax($taxId);
                     if ($product == null || $tax == null) {
+                        $this->fail(new APIError("Unknown product or tax"));
                         break;
                     }
                     $newLine = new TicketLine($number, $product,
@@ -232,6 +234,7 @@ class TicketsAPI extends APIService {
                             && TicketsService::save($ticket, $locationId)) {
                         $successes++;
                     } else {
+                        $this->fail(new APIError("Unable to edit ticket"));
                         break;
                     }
                 } else {
@@ -239,6 +242,7 @@ class TicketsAPI extends APIService {
                     if (TicketsService::save($ticket, $locationId)) {
                         $successes++;
                     } else {
+                        $this->fail(new APIError("Unable to save ticket"));
                         break;
                     }
                 }
