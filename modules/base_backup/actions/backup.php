3224bd7a4cc4fdb4cccdf1e5bb14c4ca53473e67
philippe <philippe@my.pasteque.coop>
2015-03-24 09:18:23 +0100

    Backup system improvment

--- modules/base_backup/actions/backup.php
+++ modules/base_backup/actions/backup.php
@@ -23,15 +23,35 @@ namespace BaseBackup;
 ?><h1><?php \pi18n("Backup", PLUGIN_NAME); ?></h1>
 <div class="hint"><?php \pi18n("Backup_help", PLUGIN_NAME); ?></div><?php
 
-$user_id = \Pasteque\get_user_id();
+$user_id        = \Pasteque\get_user_id();
 $dbhost = \Pasteque\get_db_host($user_id);
 $dbuser = \Pasteque\get_db_user($user_id);
-$dbpasswd = \Pasteque\get_db_password($user_id);
-$database = \Pasteque\get_db_name($user_id);
+$dbpasswd       = \Pasteque\get_db_password($user_id);
+$database       = \Pasteque\get_db_name($user_id);
 $dbport = \Pasteque\get_db_port($user_id);
 
-$cmd = "mysqldump -u " . $dbuser . " --password=" . $dbpasswd . " " . $database ." --port=" . $dbport;
+// Looking for an existing file (we do only allow one dump per day)
+$dir = opendir("cache");
+while(($f = readdir($dir)) != false) {
+    if(preg_match("/pasteque-" . date("Ymd") . "-" . $database . "-.*.sql.gz/",$f) == 1) {
+        $filename = "cache/" . $f;
+        if(time() - filemtime($filename) > 86400) {
+            $cmd = "rm cache/" . $filename;
+            exec($cmd);
+            $cmd = "mysqldump -u " . $dbuser . " --password=" . $dbpasswd . " " . $database ." --port=" . $dbport . "|gzip -c > " . $filename;
+            exec($cmd);
+        }
+        break;
+    }
+}
+// We didn’t find a file, we generate one with a little obfuscation for security
+if($filename == "") {
+    $filename       = "cache/pasteque-" . date("Ymd") . "-" . $database . "-" . md5(time() . rand(0,getrandmax())) . ".sql.gz";
+    $cmd = "mysqldump -u " . $dbuser . " --password=" . $dbpasswd . " " . $database ." --port=" . $dbport . "|gzip -c > " . $filename;
+    exec($cmd);
 
-?><textarea style="width:100%;height:80em;"><?php
-system($cmd);
-?></textarea><?php
+}
+if(file_exists($filename)) {
+    \Pasteque\tpl_btn('btn bt_export ', $filename,
+        \i18n('Download backup', PLUGIN_NAME), 'img/btn_add.png');
+}
