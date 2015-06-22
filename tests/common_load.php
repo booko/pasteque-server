5e7a18f24a69173a276d2b0f8ed7c553f0e5bbac
Nicolas Svirchevsky <n.svirchevsky@gmail.com>
2015-06-22 16:32:23 +0100

    clean better (but too much ..)

--- tests/common_load.php
+++ tests/common_load.php
@@ -34,7 +34,8 @@ function dropDatabase() {
     $pdo = PDOBuilder::getPdo();
     if ($config['db_type'] == "mysql") {
         $sqls = array("DROP TABLE APPLICATIONS;", "DROP TABLE ROLES;",
-               "DROP TABLE PEOPLE;", "DROP TABLE RESOURCES;",
+            "DROP TABLE ORDERS;",  "DROP TABLE ORDERLINES", "DROP TABLE TARIFFAREAS",  
+            "DROP TABLE PEOPLE;", "DROP TABLE RESOURCES;",
                 "DROP TABLE PROVIDERS;",
                 "DROP TABLE DISCOUNTPROFILES;",
                 "DROP TABLE TAXCUSTCATEGORIES;", "DROP TABLE CUSTOMERS;",
