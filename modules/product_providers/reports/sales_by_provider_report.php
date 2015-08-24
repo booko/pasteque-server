3ac6deff770477b8463cdf8800a539879c344ceb
philippe <philippe@my.pasteque.coop>
2015-08-24 13:48:32 +0200

    Fixed sales by provider report

--- modules/product_providers/reports/sales_by_provider_report.php
+++ modules/product_providers/reports/sales_by_provider_report.php
@@ -18,53 +18,60 @@
 //    You should have received a copy of the GNU General Public License
 //    along with Pastèque.  If not, see <http://www.gnu.org/licenses/>.
 
-namespace BaseSales;
+namespace ProductProviders;
 
 $sql = "SELECT "
         . "PRODUCTS.REFERENCE, "
         . "PRODUCTS.NAME, "
         . "PRODUCTS.PROVIDER, "
+        . "PROVIDERS.NAME AS PROVNAME, "
         . "SUM(TICKETLINES.UNITS) AS UNITS, "
-        . "SUM(TICKETLINES.UNITS * TICKETLINES.PRICE) AS TOTAL, "
-        . "SUM(TICKETLINES.UNITS * (TICKETLINES.PRICE - PRODUCTS.PRICEBUY)) AS MARGIN "
-        . "FROM RECEIPTS, TICKETS, TICKETLINES, PRODUCTS, CLOSEDCASH "
+        . "(SUM(TICKETLINES.UNITS) * TICKETLINES.PRICE) AS TOTAL, "
+        . "(SUM(TICKETLINES.UNITS) * (TICKETLINES.PRICE - PRODUCTS.PRICEBUY)) AS MARGIN, "
+        . "(SUM(TICKETLINES.UNITS) * (TICKETLINES.PRICE * (SELECT SUM(1 + TAXES.RATE) FROM TAXES WHERE PRODUCTS.TAXCAT = TAXES.CATEGORY AND TAXES.VALIDFROM < RECEIPTS.DATENEW ORDER BY TAXES.VALIDFROM DESC LIMIT 0,1))) AS TAXEDTOTAL "
+        . "FROM CLOSEDCASH "
+        . "JOIN RECEIPTS ON RECEIPTS.MONEY = CLOSEDCASH.MONEY "
+        . "JOIN TICKETS ON TICKETS.ID = RECEIPTS.ID "
+        . "JOIN TICKETLINES ON TICKETLINES.TICKET = RECEIPTS.ID "
+        . "JOIN PRODUCTS ON PRODUCTS.ID = TICKETLINES.PRODUCT "
+        . "JOIN PROVIDERS ON PROVIDERS.ID = PRODUCTS.PROVIDER "
         . "WHERE "
         . "CLOSEDCASH.DATESTART > :start AND CLOSEDCASH.DATEEND < :stop "
-        . "AND RECEIPTS.MONEY = CLOSEDCASH.MONEY "
-        . "AND RECEIPTS.ID = TICKETS.ID AND TICKETS.ID = TICKETLINES.TICKET "
-        . "AND TICKETLINES.PRODUCT = PRODUCTS.ID "
         . "GROUP BY PRODUCTS.REFERENCE, PRODUCTS.NAME, PRODUCTS.PROVIDER "
-        . "ORDER BY PRODUCTS.NAME ";
+        . "ORDER BY PROVIDERS.NAME, PRODUCTS.NAME";
 
 
-$fields = array("REFERENCE", "NAME", "UNITS", "TOTAL","MARGIN");
-$mergeFields= array("PROVIDER");
+$fields = array("REFERENCE", "NAME", "UNITS", "TOTAL", "TAXEDTOTAL", "MARGIN");
 
 $headers = array(\i18n("Product.reference"),
         \i18n("Product.label"),
-        \i18n("Quantity"), \i18n("Total w/o VAT", PLUGIN_NAME),
+        \i18n("Quantity"),
+        \i18n("Total w/o VAT", PLUGIN_NAME),
+        \i18n("Total", PLUGIN_NAME),
         \i18n("Margin", PLUGIN_NAME));
 
-$report = new \Pasteque\MergedReport(PLUGIN_NAME, "sales_by_provider_report",
+$report = new \Pasteque\Report(PLUGIN_NAME, "sales_by_provider_report",
         \i18n("Sales by provider", PLUGIN_NAME),
-        $sql, $headers, $fields, $mergeFields);
+        $sql, $headers, $fields);
 
 $report->addInput("start", \i18n("Session.openDate"), \Pasteque\DB::DATE);
 $report->setDefaultInput("start", time() - (time() % 86400) - 7 * 86400);
 $report->addInput("stop", \i18n("Session.closeDate"), \Pasteque\DB::DATE);
 $report->setDefaultinput("stop", time() - (time() % 86400) + 86400);
 
-$report->setGrouping("PROVIDER");
-$report->addSubTotal("TOTAL");
-$report->addSubTotal("MARGIN");
+$report->setGrouping("PROVNAME");
+$report->addSubTotal("TOTAL",\Pasteque\Report::TOTAL_SUM);
+$report->addSubTotal("MARGIN",\Pasteque\Report::TOTAL_SUM);
 $report->addFilter("DATESTART", "\Pasteque\stdtimefstr");
 $report->addFilter("DATESTART", "\i18nDatetime");
 $report->addFilter("DATEEND", "\Pasteque\stdtimefstr");
 $report->addFilter("DATEEND", "\i18nDatetime");
-$report->setVisualFilter("UNITS", "\i18nCurr", \Pasteque\Report::DISP_USER);
-$report->setVisualFilter("UNITS", "\i18nCurr", \Pasteque\Report::DISP_CSV);
+$report->setVisualFilter("UNITS", "\i18nInt", \Pasteque\Report::DISP_USER);
+$report->setVisualFilter("UNITS", "\i18nInt", \Pasteque\Report::DISP_CSV);
 $report->setVisualFilter("TOTAL", "\i18nCurr", \Pasteque\Report::DISP_USER);
 $report->setVisualFilter("TOTAL", "\i18nFlt", \Pasteque\Report::DISP_CSV);
+$report->setVisualFilter("TAXEDTOTAL", "\i18nCurr", \Pasteque\Report::DISP_USER);
+$report->setVisualFilter("TAXEDTOTAL", "\i18nFlt", \Pasteque\Report::DISP_CSV);
 $report->setVisualFilter("MARGIN", "\i18nCurr", \Pasteque\Report::DISP_USER);
 $report->setVisualFilter("MARGIN", "\i18nFlt", \Pasteque\Report::DISP_CSV);
 
